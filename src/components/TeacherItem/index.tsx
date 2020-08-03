b1bfe5646fea750b1e4f5dafb4d7f3219a0a052b
rodrigoabam <rodrigoab.am@gmail.com>
2020-08-03 19:31:39 -0400

    primeiro dia nlw omni

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css'
+
+function TeacherItem() {
+    return (
+        <article className="teacher-item">
+                <header>
+                    <img src="https://avatars2.githubusercontent.com/u/62651527?s=460&u=88f5d7501ea4e07aad56e509bcfe886288dba427&v=4" alt="Rodrigo"/>
+                    <div>
+                        <strong>Rodrigo de Almeida</strong>
+                        <span>Física</span>
+                    </div>
+                </header>
+                <p>
+                    Entusiasta das melhores tecnologias de física avançada.
+                    <br/><br/>
+                    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. 
+                    Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                </p>
+
+                <footer>
+                    <p>Preço/hora <strong>R$ 20,00</strong></p>
+                    <button type="button">
+                        <img src={whatsappIcon} alt="Whatsapp"/>
+                        Entrar em contato
+                    </button>
+                </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
