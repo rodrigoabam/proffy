ab79b22ccdbe508d2a521b81a28579350ccb859f
rodrigoabam <rodrigoab.am@gmail.com>
2020-08-06 02:47:19 -0400

    dia 3 ligando front com o back

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,32 +1,53 @@
 import React from 'react';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+};
+
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+
     return (
         <article className="teacher-item">
                 <header>
-                    <img src="https://avatars2.githubusercontent.com/u/62651527?s=460&u=88f5d7501ea4e07aad56e509bcfe886288dba427&v=4" alt="Rodrigo"/>
+                    <img src={ teacher.avatar } alt={ teacher.name }/>
                     <div>
-                        <strong>Rodrigo de Almeida</strong>
-                        <span>Física</span>
+                        <strong>{ teacher.name }</strong>
+                        <span>{ teacher.subject }</span>
                     </div>
                 </header>
                 <p>
-                    Entusiasta das melhores tecnologias de física avançada.
-                    <br/><br/>
-                    Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. 
-                    Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                { teacher.bio }
                 </p>
 
                 <footer>
-                    <p>Preço/hora <strong>R$ 20,00</strong></p>
-                    <button type="button">
+                    <p>Preço/hora <strong>R$ { teacher.cost }</strong></p>
+                    <a 
+                        target="_blank" 
+                        onClick={ createNewConnection } 
+                        href={`https://wa.me/${ teacher.whatsapp }`}>
                         <img src={whatsappIcon} alt="Whatsapp"/>
                         Entrar em contato
-                    </button>
+                    </a>
                 </footer>
         </article>
     )
